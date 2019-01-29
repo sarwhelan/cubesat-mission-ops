fa36fe848d5f894da8b90b77991f07f4706e336f
NickVanBommel <nvanbomm@uwo.ca>
2019-01-29 15:34:40 -0500

    Added TTQ route and components

---
+++ frontend/mission-ops/src/app/execution-queue/execution-queue.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-execution-queue',
+  templateUrl: './execution-queue.component.html',
+  styleUrls: ['./execution-queue.component.scss']
+})
+export class ExecutionQueueComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
