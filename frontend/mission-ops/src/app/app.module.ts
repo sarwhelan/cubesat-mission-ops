dcc638fbad131b7811a207d834f8b1b8470560a1
unknown <tmachad3@gmail.com>
2019-01-30 11:24:55 -0500

    Added FormsModule to global imports

--- frontend/mission-ops/src/app/app.module.ts
+++ frontend/mission-ops/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,7 +29,8 @@ import { TransmissionQueueComponent } from './transmission-queue/transmission-qu
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
