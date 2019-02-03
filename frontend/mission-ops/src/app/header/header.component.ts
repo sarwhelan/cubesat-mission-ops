cab6c128274f24317f4ef8e4cb5208ec48c97b54
NickVanBommel <nvanbomm@uwo.ca>
2019-02-03 12:43:04 -0500

    Adding bootstrap and a real header
    Added bootstrap package and made the header use bootstrap

--- frontend/mission-ops/src/app/header/header.component.ts
+++ frontend/mission-ops/src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HeaderItem } from '../../classes/headerItem';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  activeHeader: HeaderItem;
+  headers: HeaderItem[] = [
+    new HeaderItem("Queue", "/queue"),
+    new HeaderItem("Telecommands", "/telecommands"),
+    new HeaderItem("Telecommand Batches", "/telecommandBatches"),
+  ];
+
+  constructor(private router: Router) {  
+    
+    // This allows the active header to be selected even when the page is refreshed
+    this.router.events.subscribe(val => {
+      if (this.activeHeader == null)
+      {
+        for (let header of this.headers)
+        {
+          if (header.route == this.router.url)
+          {
+            this.activeHeader = header;
+            break;
+          }
+        }
+      }
+    });
+  }
 
   ngOnInit() {
+  }
+
+  onSelect(newActiveHeader: HeaderItem): void{
+    this.activeHeader = newActiveHeader;
   }
 
 }
