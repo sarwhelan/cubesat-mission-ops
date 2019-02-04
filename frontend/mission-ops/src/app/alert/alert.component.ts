2b7d1e19b31d8e09c242f335b18f80e6658cf8d5
Tom Machado <tmachad3@gmail.com>
2019-02-03 20:22:59 -0500

    Added alert component documentation

--- frontend/mission-ops/src/app/alert/alert.component.ts
+++ frontend/mission-ops/src/app/alert/alert.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+/**
+ * A component representing a standard dismissable bootstrap alert with a heading and body.
+ * To include in html, use `<app-alert [alertStyle]="'danger'"></app-alert>`, where danger 
+ * can be replaced with any valid bootstrap style (primary, secondary, success, etc). To 
+ * show and hide, add `@ViewChild(AlertComponent) alertVarName: AlertComponent` to the 
+ * parent component to obtain a reference to the alert component, then just call the 
+ * component's show and hide methods using that reference.
+ *
+ * @export
+ * @class AlertComponent
+ * @implements {OnInit}
+ */
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.component.html',
@@ -28,20 +40,49 @@ export class AlertComponent implements OnInit {
   ngOnInit() {
   }
 
-  public show(heading: string, body: string): void {
+  /**
+   * Shows the alert and sets its properties to the provided values.
+   * Displays the body as a simple paragraph.
+   *
+   * @param {string} heading The heading for the alert to display.
+   * @param {string} body The body of the alert.
+   * @param {string} [alertStyle] The style to set the alert to. Can be any valid bootstrap style (primary, secondary, success, etc).
+   * @memberof AlertComponent
+   */
+  public show(heading: string, body: string, alertStyle?: string): void {
     this.listBody = false;
     this.heading = heading;
     this.body = body;
+    if (alertStyle) {
+      this.alertStyle = alertStyle;
+    }
     this.showAlert = true;
   }
 
-  public showList(heading: string, body: Array<string>): void {
+  /**
+   * Shows the alert and sets its properties to the provided values.
+   * Displays the body as an unordered list.
+   *
+   * @param {string} heading The heading for the alert to display.
+   * @param {Array<string>} body The list of items that make up the body of the alert.
+   * @param {string} [alertStyle] The style to set the alert to. Can be any valid bootstrap style (primary, secondary, success, etc).
+   * @memberof AlertComponent
+   */
+  public showList(heading: string, body: Array<string>, alertStyle?: string): void {
     this.listBody = true;
     this.heading = heading;
     this.bodyList = body;
+    if (alertStyle) {
+      this.alertStyle = alertStyle;
+    }
     this.showAlert = true;
   }
   
+  /**
+   * Hides the alert.
+   *
+   * @memberof AlertComponent
+   */
   public hide(): void {
     this.showAlert = false;
   }
