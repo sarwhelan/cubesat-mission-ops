03d8fc3ea635b5d61b6d0cd97927b883a54618fc
dzagar <10978376+dzagar@users.noreply.github.com>
2019-02-05 08:33:07 -0500

    Added POST forwarding to app server
    Forwarding CubeSat data dump in POST request to new POST request for
    app server.

--- ground_station/routes/cubesat_dump.js
+++ ground_station/routes/cubesat_dump.js
@@ -3,13 +3,35 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const parseUrlencoded = bodyParser.urlencoded({extended: false});
 const parseJSON = bodyParser.json();
+const http = require('http');
+const logger = require('../logger');
 
 router.route('/')
     .post(parseUrlencoded, parseJSON, (req, res) => {
-        console.log('POST DUMP');
-        res.send('Success');
-        // Save dump to external
-        // Forward to application server
+    	console.log('rcvd post');
+        var options = {
+        	host: "127.0.0.1",
+        	port: "3000",
+        	path: "/gs-template",
+        	method: "POST",
+        	headers: {
+        		"Content-Type": "application/json",
+        		"Authorization": "Bearer token"
+        	}
+        };
+        var fwdReq = http.request(options, function(fwdRes) {
+        	console.log('entered cb')
+        	var respStr = "";
+        	fwdRes.on("data", function(data) {
+        		respStr += data;
+        	});
+        	fwdRes.on("end", function() {
+        		console.log(respStr);
+        		res.send('Success');
+        	});
+        });
+        fwdReq.write(JSON.stringify(req.body));
+        fwdReq.end();
     });
 
 module.exports = router;
