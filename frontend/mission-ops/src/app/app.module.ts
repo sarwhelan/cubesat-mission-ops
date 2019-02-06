22731c2e1674e513a623c428b685d53f0ca5419a
Tom Machado <tmachad3@gmail.com>
2019-02-06 15:12:19 -0500

    Added ng-bootstrap package

--- frontend/mission-ops/src/app/app.module.ts
+++ frontend/mission-ops/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; // TODO: Before deployment, maybe refine this to only the modules we need to reduce package size
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -32,7 +33,8 @@ import { AlertComponent } from './alert/alert.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
