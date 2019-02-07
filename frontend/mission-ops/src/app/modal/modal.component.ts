ef17e3817f55250862ce1b5864168feece57f741
Tom Machado <tmachad3@gmail.com>
2019-02-06 20:40:37 -0500

    Refined modal for generic data source

--- frontend/mission-ops/src/app/modal/modal.component.ts
+++ frontend/mission-ops/src/app/modal/modal.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
 import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { IObservable } from 'src/interfaces/iObservable';
 
 /**
  * A component representing a standard bootstrap modal window with a title, body, and footer.
  * To use this component, insert the following html code into your template:
  *
- * <app-modal>
+ * <app-modal [dataSource]="mycomponent">
  *  <ng-container modal-title>
  *    <!-- any html you want to appear in the title bar -->
  *  </ng-container modal-title>
@@ -38,23 +39,59 @@ export class ModalComponent implements OnInit {
 
   private modalRef: NgbModalRef;
 
+  @Input()
+  private dataSource: IObservable<any>;
+
   constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
   }
 
+  /**
+   * Opens the modal window and returns a promise that will eventually
+   * complete with the result of the modal. The promise will resolve if
+   * the modal was closed (i.e. operations completed successfully) and
+   * will reject if the modal was dismissed (i.e. the user closed it
+   * manually or the operation failed).
+   *
+   * @returns {Promise<any>} A promise containing the result of the operation performed by the modal.
+   * @memberof ModalComponent
+   */
   public open(): Promise<any> {
     this.modalRef = this.modalService.open(this.modalContent);
     
+    if (this.dataSource) {
+      this.dataSource.getObservable().subscribe((result) => {
+        this.close(result);
+      },
+      (err) => {
+        this.dismiss(err);
+      });
+    }
+
     return this.modalRef.result;
   }
 
+  /**
+   * Closes the modal and causes the modal promise to reject with
+   * the given reason.
+   *
+   * @param {*} [reason] The reason the modal was dismissed.
+   * @memberof ModalComponent
+   */
   public dismiss(reason?: any) {
     if (this.modalRef) {
       this.modalRef.dismiss(reason);
     }
   }
 
+  /**
+   * Closes the modal and causes the modal promise to resolve
+   * with the given result.
+   *
+   * @param {*} [result] The result returned by the modal promise.
+   * @memberof ModalComponent
+   */
   public close(result?: any) {
     if (this.modalRef) {
       this.modalRef.close(result);
