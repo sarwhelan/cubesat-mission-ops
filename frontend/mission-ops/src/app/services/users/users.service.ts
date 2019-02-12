d8c27f064aaeac807b5bd66ca56f4530d0a76514
Tom Machado <tmachad3@gmail.com>
2019-02-11 21:43:59 -0500

    Added documentation to users service

--- frontend/mission-ops/src/app/services/users/users.service.ts
+++ frontend/mission-ops/src/app/services/users/users.service.ts
@@ -6,6 +6,12 @@ export interface ICreateUserCallback {
   onFailure: (err: any) => void
 }
 
+/**
+ * A service for creating, getting, updating, and listing users.
+ *
+ * @export
+ * @class UsersService
+ */
 @Injectable({
   providedIn: 'root'
 })
@@ -22,6 +28,17 @@ export class UsersService {
     });
   }
 
+  /**
+   * Creates a new user using the given parameters, and calls the given
+   * callbacks depending on the success of the operation.
+   *
+   * @param {string} username The username of the user. This can be the same as the user's email.
+   * @param {string} password A temporary password for the user. The user will be forced to change it on their first login. If left blank, a temporary password will be automatically generated.
+   * @param {string} email The user's email address. A welcome email with the user's username and password will be sent to this address once user creation succeeds.
+   * @param {boolean} admin Whether or not this user is an administrator. This will be used to determine whether or not they have access to certain features.
+   * @param {ICreateUserCallback} callback An object containing the callback methods called when the operation succeeds or fails.
+   * @memberof UsersService
+   */
   public createUser(username: string, password: string, email: string, admin: boolean, callback: ICreateUserCallback): void {
     // TODO: Validate input
 
@@ -55,7 +72,16 @@ export class UsersService {
     });
   }
 
-  public getUsers(limit: number = 10, attributesToGet: Array<string> = null): void {
+  /**
+   * WORK IN PROGRESS
+   * Gets the list of all users that exist in the user pool. Currently returns nothing,
+   * but at some point will allow for accessing paginated results somehow.
+   *
+   * @param {number} [limit=10] The number of users to retrieve.
+   * @param {Array<string>} [attributesToGet=null] The attributes to get for each user. If unspecified, all attributes are retrieved.
+   * @memberof UsersService
+   */
+  public listUsers(limit: number = 10, attributesToGet: Array<string> = null): void {
     this.cognitoIdentityServiceProvider.listUsers({
       UserPoolId: 'us-east-2_eniCDFvnv',
       AttributesToGet: attributesToGet,
