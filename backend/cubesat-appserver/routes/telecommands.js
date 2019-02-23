b13c61d6be6ba1dfffa4ba652b453afeaee0c4cc
NickVanBommel <nvanbomm@uwo.ca>
2019-02-23 15:21:26 -0500

    Connected backend to database
    FINALLY CONNECTED IT TO THE DB

--- backend/cubesat-appserver/routes/telecommands.js
+++ backend/cubesat-appserver/routes/telecommands.js
@@ -3,22 +3,21 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const parseUrlencoded = bodyParser.urlencoded({extended: false});
 const parseJSON = bodyParser.json();
+var db = require('../database');
 
 router.route('/')
     .get(parseUrlencoded, parseJSON, (req, res) => {
         try {
-            response = {
-                'statusCode': 200,
-                'body': JSON.stringify({
-                    message: 'This is the get route'
-                })
-            };
+            var response;
+            db.query("SELECT * FROM systems;", function (error, results, fields) {
+                if (error) throw error;
+                console.log(results);
+                res.send("The result is " + results[0].systemName);
+              });
         } catch (err) {
             console.log(err);
             res.send(err);
         }
-    
-        res.send(response);
     })
     .post(parseUrlencoded, parseJSON, (req, res) => {
         try {
