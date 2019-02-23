fbd63157af7b912f89acd01447342a21e502d32e
Dana Zagar <10978376+dzagar@users.noreply.github.com>
2019-02-22 22:57:52 -0500

    Fixed CubeSat dump to app server routing

--- ground_station/data_dump/poll_cubesat_dump.js
+++ ground_station/data_dump/poll_cubesat_dump.js
@@ -1,61 +1,86 @@
 var path = require('path');
 var fs = require('fs');
 var request = require('request');
-var Inotify = require('inotify').Inotify();
+var Inotify = require('inotify').Inotify;
 var inotify = new Inotify();
+var http = require('http');
+var logger = require('../logger');
 
-const DUMP_DIR = "/Users/danazagar/Documents/Cappy/dump_test";
-
-var data = {};
+const DUMP_DIR = '/home/dzagar/Desktop/dumptest';
+const WATCH_DIR = '/home/dzagar/Desktop/watchtest';
 
 var callback = function(event) {
+    console.log('success');
     var mask = event.mask;
-    var type = 'file';
+    var type = '';
     if (event.name) {
-        type += ' ' + event.name + ' ';
+        type += event.name;
     } else {
-        type += ' ';
+        type += 'n/a';
     }
+	console.log(type);
+	var srcFilePath = path.join(WATCH_DIR, type);
 
     if (mask & Inotify.IN_ISDIR) {
         return;
     }
-    if (mask & Inotify.IN_CREATE) {
-        inotify.addWatch({
+    if (mask & Inotify.IN_CLOSE_WRITE) {
+        /*inotify.addWatch({
             path: type,
             watch_for: Inotify.IN_CLOSE_WRITE,
             callback: callback
-        });
+        });*/
         console.log(`Received created event ${type}`);
-        var date = new Date().getDate();
-        console.log(date);
-        subDirPath = path.join(DUMP_DIR, date);
+        var date = new Date();
+		var dateString = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate();
+        console.log(dateString);
+        subDirPath = path.join(DUMP_DIR, dateString);
         if (!fs.existsSync(subDirPath)) {
             fs.mkdirSync(subDirPath);
         }
-        fs.copyFile(type, path.join(subDirPath, path.basename(type)), (err) => {
+        fs.copyFile(srcFilePath, path.join(subDirPath, path.basename(type)), (err) => {
             if (err) throw err;
             console.log('Copy successful');
         });
-        fs.createReadStream(type)
-        .pipe(
-            request.post(
-            "http://127.0.0.1:3700/cubesat_dump"
-            ).on('error', err => {
-                console.log(err);
-            })
-            .on('response', resp => {
-                console.log(resp.statusCode);
-                console.log(resp.headers['content-type']);
-            })
-        );
+
+		var options = {
+        	host: "127.0.0.1",
+        	port: "3000",
+        	path: "/cubesat_dump",
+        	method: "POST",
+        	headers: {
+        		"Content-Type": "application/json",
+        		"Authorization": "Bearer token",
+        		"Access-Control-Allow-Origin": "*"
+        	}
+        };
+		var jsonDump = JSON.stringify(JSON.parse(fs.readFileSync(srcFilePath)));
+		var fwdReq = http.request(options, res => {
+        	var respStr = "";
+			res.on('error', err => {
+				console.log(err);
+			});
+			res.on('data', data => {
+				respStr += data;
+			});
+			res.on('end', () => {
+				console.log(respStr);
+				if (!res.complete) {
+					console.log('but why');
+				}
+			});
+        });
+		fwdReq.on('error', err => {
+			console.log(err);
+		});
+		fwdReq.write(jsonDump);
+        fwdReq.end();
         
     }
 }
 
 var watch_dir = {
-    path: '/Users/danazagar/Documents/Cappy/watch_test',
-    watch_for: Inotify.IN_ALL_EVENTS,
+    path: WATCH_DIR,
     callback: callback
 };
 
