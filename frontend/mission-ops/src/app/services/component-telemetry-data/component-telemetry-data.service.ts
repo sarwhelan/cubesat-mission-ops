4ae06451795f6295f17461286525822b387a5932
dzagar <10978376+dzagar@users.noreply.github.com>
2019-02-28 18:37:06 -0500

    Completed component telemetry, telemetry data services
    :palm_tree:

--- frontend/mission-ops/src/app/services/component-telemetry-data/component-telemetry-data.service.ts
+++ frontend/mission-ops/src/app/services/component-telemetry-data/component-telemetry-data.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TelemetryData } from '../../../classes/telemetry-data';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ComponentTelemetryDataService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private telemetryDataUrl = "http://localhost:3000/telemetry-data";
+
+  getTelemetryData(componentTelemetryID: number): Observable<TelemetryData[]> {
+    var getTelemetryDataUrl = this.telemetryDataUrl + "/" + componentTelemetryID;
+    return this.http.get<TelemetryData[]>(getTelemetryDataUrl);
+  }
 }
--- frontend/mission-ops/src/app/services/component-telemetry/component-telemetry.service.ts
+++ frontend/mission-ops/src/app/services/component-telemetry/component-telemetry.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ComponentTelemetry } from '../../../classes/component-telemetry';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ComponentTelemetryService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private componentTelemetryUrl = "http://localhost:3000/component-telemetry";
+
+  getComponentTelemetries(componentID: number) : Observable<ComponentTelemetry[]>
+  {
+    var getComponentUrl = this.componentTelemetryUrl + "/" + componentID;
+    return this.http.get<ComponentTelemetry[]>(getComponentUrl);
+  }
 }
