b5f7237a74e976aa4060df6215d9f8fa6afcad3d
Sarah Whelan <sarwhelan11@gmail.com>
2019-03-04 17:52:37 -0500

    structure of post

--- backend/cubesat-appserver/routes/telem-alerts.js
+++ backend/cubesat-appserver/routes/telem-alerts.js
@@ -1,8 +1,10 @@
-// telem-alerts filters through new datasets sent from the ground station
-// to determine if any telemetry values fall outside the pre-defined telemetry
-// limits (both lower and upper bounds). if so, an SMS will be sent to all 
-// users subscribed to that component. additionally, each anomaly will be 
-// stored separately for easy future reference.
+/* 
+    telem-alerts filters through new datasets sent from the ground station
+    to determine if any telemetry values fall outside the pre-defined telemetry
+    limits (both lower and upper bounds). if so, an SMS will be sent to all 
+    users subscribed to the system associated with the componentTelemetry. 
+    additionally, each anomaly will be stored separately for easy future reference.
+ */
 
 const express = require('express');
 const router = express.Router();
@@ -20,31 +22,38 @@ const twilioAuthToken = process.env.TWILIO_AUTH_TOKEN
 const twilioPhoneNum = process.env.TWILIO_NUM
 const client = require('twilio')(accountSid, twilioAuthToken);
 
+
 router.route('/')
     .post(parseUrlencoded, parseJSON, (req, res) => { // POST /telem-alerts receives a fresh batch of data from the ground station to be processed
 
         // cycle through all new data checking for anomalies
-        // res.send(req.body);
+        for (telem in req.body.reading) {
+
+        }
 
         // if anomaly...
         // get users subscribed to component whose telemetry item has an anomaly
 
         // if anomaly, get phone number of all users subscribed to 
+        var phoneNum;
+
         try {
-            var phoneNum = db.query('SELECT phoneNumber FROM users WHERE userID = ?', req.params.userID, function (error, results, fields) {
+            db.query('SELECT phoneNumber FROM users WHERE userID = ?', userID, function (error, results, fields) {
                 if (error) throw error;
+                phoneNum = results[0].phoneNumber;
+
+                // then send SMS alert to all phoneNums
+                client.messages.create({
+                    body: 'This is a CubeSat Telemetry Alert!!!',
+                    from: twilioPhoneNum,
+                    to: phoneNum
+                }).then(message => console.log(message.sid));
+
             });
         } catch (err) {
             console.log(err);
             res.send(err);
         }
-
-        // then send SMS alert to all phoneNums
-        client.messages.create({
-            body: 'This is a CubeSat Telemetry Alert!!!',
-            from: twilioPhoneNum,
-            to: phoneNum
-        })
 
     })
 
@@ -58,4 +67,4 @@ router.route('/')
 
     })
 
-    module.exports = router;
+module.exports = router;
--- backend/cubesat-appserver/server.js
+++ backend/cubesat-appserver/server.js
@@ -8,10 +8,13 @@ var telecommands = require('./routes/telecommands');
 var template = require('./routes/template');
 var db_template = require('./routes/db-template');
 var cubesat_dump = require('./routes/cubesat_dump');
-var modify_telem_limit  = require('./routes/modify-telem-limit');
 var components = require('./routes/components');
 var componentTelemetry = require('./routes/component-telemetry');
 var telemetryData = require('./routes/telemetry-data');
+
+// routes related to telemetry limits and corresponding alerts
+var telemAlert = require('./routes/telem-alerts');
+
 const logger = require('./logger');
 
 const port = 3000;
@@ -29,10 +32,12 @@ app.use('/telecommands', telecommands);
 app.use('/template', template);
 app.use('/db-template', db_template);
 app.use('/cubesat_dump', cubesat_dump);
-app.use('/modify-telem-limit', modify_telem_limit);
 app.use('/components', components);
 app.use('/component-telemetry', componentTelemetry);
 app.use('/telemetry-data', telemetryData);
+app.use('/telem-alerts', telemAlert);
+
+
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
 
