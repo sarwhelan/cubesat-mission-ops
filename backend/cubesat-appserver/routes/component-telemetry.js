a13e0fc99bb7a9e9891448827dc6300959887bd3
Sarah Whelan <sarwhelan11@gmail.com>
2019-03-06 16:10:07 -0500

    routes for componentTelemetry added and tested

--- backend/cubesat-appserver/routes/component-telemetry.js
+++ backend/cubesat-appserver/routes/component-telemetry.js
@@ -6,6 +6,7 @@ const parseJSON = bodyParser.json();
 var db = require('../database');
 
 router.route('/')
+    // GET /component-telemetry returns all componentTelemetry entries
     .get(parseUrlencoded, parseJSON, (req, res) => {
         try {
             db.query("SELECT * FROM componentTelemetry", function (error, results, fields) {
@@ -17,24 +18,27 @@ router.route('/')
             res.send(err);
         }
     })
+
+    // POST /component-telemetry creates a new componentTelemetry as specified by telemetryTypeID
     .post(parseUrlencoded, parseJSON, (req, res) => {
         try {
-            var insertParams = [req.body.telemetryTypeID, req.body.componentID];
-            db.query('INSERT INTO componentTelemetry (telemetryTypeID, componentID) VALUES (?, ?)', insertParams, (error, results, fields) => {
+            // NOTE upperBound and lowerBound can be null
+            var insertParams = [req.body.telemetryTypeID, req.body.componentID, req.body.name, req.body.upperBound, req.body.lowerBound];
+            db.query('INSERT INTO componentTelemetry (telemetryTypeID, componentID, name, upperBound, lowerBound) VALUES (?, ?, ?, ?, ?)', insertParams, (error, results, fields) => {
                 if (error) throw error;
-                console.log('Inserted component telemetry successfully');
-                res.json(results);
+                res.json(200);
             });
         } catch (err) {
             console.log(err);
             res.json(err);
         }
-    });
+    })
 
-router.route('/:componentID')
+router.route('/:ID')
+    // GET /component-telemetry/:componentID returns all componentTelemetries associated with the componentID parameter specified
     .get(parseUrlencoded, parseJSON, (req, res) => {
         try {
-            db.query("SELECT * FROM componentTelemetry WHERE componentID = ?;", req.params.componentID, function (error, results, fields) {
+            db.query("SELECT * FROM componentTelemetry WHERE componentID = ?;", req.params.ID, function (error, results, fields) {
                 if (error) throw error;
                 res.send(results);
               });
@@ -42,6 +46,35 @@ router.route('/:componentID')
             console.log(err);
             res.send(err);
         }
-    });
+    })
+
+    // PUT /component-telemetry/:componentTelemetryID updates the fields of the componentTelemetryID specified
+    .put(parseUrlencoded, parseJSON, (req, res) => {
+        try {
+            var updateParams = [req.body.telemetryTypeID, req.body.componentID, req.body.name, req.body.upperBound, req.body.lowerBound, req.params.ID];
+
+            db.query("UPDATE componentTelemetry SET telemetryTypeID = ?, componentID = ?, name = ?, upperBound = ?, lowerBound = ? " +
+                "WHERE componentTelemetryID = ?", updateParams, function (error, results, fields) {
+                if (error) throw error;
+                res.sendStatus(200);
+              });
+        } catch (err) {
+            console.log(err);
+            res.send(err);
+        }
+    })
+
+    // DELETE /component-telemetry/:componentTelemetryID deletes the componentTelemetryID entry
+    .delete(parseUrlencoded, parseJSON, (req, res) => {
+        try {
+            db.query("DELETE FROM componentTelemetry WHERE componentTelemetryID = ?", req.params.ID, function(error, results, fields) {
+                if (error) throw error;
+                res.sendStatus(200);
+            })
+        } catch(err) {
+            console.log(err);
+            res.send(err);
+        }
+    })
 
 module.exports = router;
