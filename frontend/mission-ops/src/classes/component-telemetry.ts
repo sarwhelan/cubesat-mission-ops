f3feea0ba0e6cfdd3303a3eba29a33fb414d7e29
dzagar <10978376+dzagar@users.noreply.github.com>
2019-03-06 23:34:59 -0500

    Updated component telemetry class
    - To be consistent with new database columns

--- frontend/mission-ops/src/classes/component-telemetry.ts
+++ frontend/mission-ops/src/classes/component-telemetry.ts
@@ -2,9 +2,19 @@ export class ComponentTelemetry {
     componentTelemetryID: number;
     telemetryTypeID: number;
     componentID: number;
+    name: string;
+    upperBound: number;
+    lowerBound: number;
 
-    constructor(telemetryTypeID: number, componentID: number) {
+    constructor(telemetryTypeID: number, 
+                componentID: number,
+                name: string,
+                upperBound: number = null,
+                lowerBound: number = null) {
         this.telemetryTypeID = telemetryTypeID;
         this.componentID = componentID;
+        this.name = name;
+        this.upperBound = upperBound;
+        this.lowerBound = lowerBound;
     }
 }
