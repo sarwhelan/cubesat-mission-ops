3e56b4f0546745dcf0687b9849564099f1334557
dzagar <10978376+dzagar@users.noreply.github.com>
2019-03-07 10:16:52 -0500

    Added hasBounds attribute to telemetryTypes table
    Indicates whether or not the telemetry type has a defined numeric
    upper/lower bound

--- frontend/mission-ops/src/classes/telemetry-type.ts
+++ frontend/mission-ops/src/classes/telemetry-type.ts
@@ -2,9 +2,11 @@ export class TelemetryType {
     telemetryTypeID: number;
     telemetryUnit: string;
     name: string;
+    hasBounds: boolean;
 
-    constructor(telemetryUnit: string, name: string){
+    constructor(telemetryUnit: string, name: string, hasBounds: boolean){
         this.telemetryUnit = telemetryUnit;
         this.name = name;
+        this.hasBounds = hasBounds;
     }
 }
