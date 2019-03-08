098712c8c168456e69f3d548859f1f272fffb77b
dzagar <10978376+dzagar@users.noreply.github.com>
2019-03-08 17:54:03 -0500

    Cleaned up datetime picker
    - Removed ngModel stuff since that is against the reactive forms module way :squirrel:

--- frontend/mission-ops/src/app/datetime-picker/datetime-picker.component.ts
+++ frontend/mission-ops/src/app/datetime-picker/datetime-picker.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { formGroupNameProvider } from '@angular/forms/src/directives/reactive_directives/form_group_name';
 
 @Component({
   selector: 'app-datetime-picker',
@@ -7,11 +6,6 @@ import { formGroupNameProvider } from '@angular/forms/src/directives/reactive_di
   styleUrls: ['./datetime-picker.component.scss']
 })
 export class DatetimePickerComponent {
-  @Input()
-  date;
-
-  @Input()
-  time;
 
   @Input()
   dateFormControlName;
