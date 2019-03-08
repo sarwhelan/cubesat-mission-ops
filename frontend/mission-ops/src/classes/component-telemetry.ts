b0c1af03b85c09317c9d94664fc03e0c0871bc28
dzagar <10978376+dzagar@users.noreply.github.com>
2019-03-08 14:55:40 -0500

    Added docs for ComponentTelemetry class
    - Because gotta add all the docs :smile:

--- frontend/mission-ops/src/classes/component-telemetry.ts
+++ frontend/mission-ops/src/classes/component-telemetry.ts
@@ -1,11 +1,60 @@
 export class ComponentTelemetry {
+    /**
+     * The unique ID.
+     * 
+     * @type {number}
+     * @memberof ComponentTelemetry
+     */
     componentTelemetryID: number;
+
+    /**
+     * The ID of the @alias TelemetryType associated.
+     * 
+     * @type {number}
+     * @memberof ComponentTelemetry
+     */
     telemetryTypeID: number;
+
+    /**
+     * The ID of the @alias Component associated.
+     * 
+     * @type {number}
+     * @memberof ComponentTelemetry
+     */
     componentID: number;
+
+    /**
+     * The name of the component telemetry.
+     * 
+     * @type {string}
+     * @memberof ComponentTelemetry
+     */
     name: string;
+
+    /**
+     * The upper acceptable limit of the component telemetry data.
+     * 
+     * @type {number}
+     * @memberof ComponentTelemetry
+     */
     upperBound: number;
+
+    /**
+     * The lower acceptable limit of the component telemetry data.
+     * 
+     * @type {number}
+     * @memberof ComponentTelemetry
+     */
     lowerBound: number;
 
+    /**
+     * Creates a new instance of @member ComponentTelemetry.
+     * @param telemetryTypeID The associated telemetry type ID.
+     * @param componentID The associated component ID.
+     * @param name The name.
+     * @param upperBound [optional] The upper bound.
+     * @param lowerBound [optional] The lower bound.
+     */
     constructor(telemetryTypeID: number, 
                 componentID: number,
                 name: string,
