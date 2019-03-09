cf8ab8bf97c556b13ad9803ad43368e9c10e6cdc
dzagar <10978376+dzagar@users.noreply.github.com>
2019-03-08 23:53:37 -0500

    Added docs for component telemetry, pass services
    - ok I'M ACTUALLY GONNA SLEEP NOW!
    - Go docs go

--- frontend/mission-ops/src/app/services/component-telemetry/component-telemetry.service.ts
+++ frontend/mission-ops/src/app/services/component-telemetry/component-telemetry.service.ts
@@ -3,31 +3,61 @@ import { Observable } from 'rxjs';
 import { ComponentTelemetry } from '../../../classes/component-telemetry';
 import { HttpClient } from '@angular/common/http';
 
+/**
+ * Service handling all {@link ComponentTelemetry} app server routing.
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class ComponentTelemetryService {
 
+  /**
+   * Creates a new instance of {@link ComponentTelemetryService}.
+   * @param http The HttpClient service.
+   */
   constructor(private http: HttpClient) { }
 
+  /**
+   * Route URL for component telemetries.
+   */
   private componentTelemetryUrl = "http://localhost:3000/component-telemetry";
 
+  /**
+   * Gets all {@link ComponentTelemetry} objects associated with the given {@link Component}
+   * ID.
+   * @param componentID The ID of the {@link Component}.
+   */
   getComponentTelemetries(componentID: number) : Observable<ComponentTelemetry[]>
   {
     var getComponentUrl = this.componentTelemetryUrl + "/" + componentID;
     return this.http.get<ComponentTelemetry[]>(getComponentUrl);
   }
 
+  /**
+   * Saves the given {@link ComponentTelemetry} in the 'componentTelemetry' database 
+   * table.
+   * @param componentTelemetry The new {@link ComponentTelemetry} to save.
+   */
   createComponentTelemetry(componentTelemetry: ComponentTelemetry) : Observable<Number>
   {
     return this.http.post<Number>(this.componentTelemetryUrl, componentTelemetry);
   }
 
+  /**
+   * Updates the given {@link ComponentTelemetry} in the 'componentTelemetry' database 
+   * table.
+   * @param componentTelemetry The updated {@link ComponentTelemetry} to save.
+   */
   updateComponentTelemetry(componentTelemetry: ComponentTelemetry) : Observable<Number>
   {
     return this.http.put<Number>(`${this.componentTelemetryUrl}/${componentTelemetry.componentTelemetryID}`, componentTelemetry);
   }
 
+  /**
+   * Removes the given {@link ComponentTelemetry} from the 'componentTelemetry' database 
+   * table.
+   * @param componentTelemetry The {@link ComponentTelemetry} to remove.
+   */
   removeComponentTelemetry(componentTelemetry: ComponentTelemetry) : Observable<ComponentTelemetry>
   {
     return this.http.delete<ComponentTelemetry>(`${this.componentTelemetryUrl}/${componentTelemetry.componentTelemetryID}`);
