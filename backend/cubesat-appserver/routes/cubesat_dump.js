67c831f1b6efc731dba77bdea93dc4ca35fbec6b
Sarah Whelan <sarwhelan11@gmail.com>
2019-03-10 14:05:05 -0400

    registering that pass has occurred when loading new data dump

--- backend/cubesat-appserver/routes/cubesat_dump.js
+++ backend/cubesat-appserver/routes/cubesat_dump.js
@@ -12,10 +12,19 @@ router.route('/')
         anomalyDetector.dataDump(req.body);
 
         try {
+            var now = new Date();
+            
+            // set next pass in line as "occurred" and update the actual pass date time of the pass
+            db.query("SELECT estimatedPassDateTime FROM passes WHERE passHasOccurred = 0 ORDER BY estimatedPassDateTime ASC", function(error, results, fields) {
+                let nextUnoccurredPass = results[0].estimatedPassDateTime;
+                db.query("UPDATE passes SET passHasOccurred = ?, actualPassDateTime = ? WHERE estimatedPassDateTime = ?", [1, now, nextUnoccurredPass], function(error, results, fields) {
+                    if (error) throw error;
+                })
+            })
 
             for (reading = 0; reading < req.body.length; reading++) {
                 let insertParams = [req.body[reading].componentTelemetryID, req.body[reading].collectionDateTime, req.body[reading].telemetryValue];
-                db.query("INSERT INTO telemetryData (componentTelemetryID, collectionDateTime, telemetryValue) VALUES (?, ?, ?)", insertParams, function(error, response, fields) {
+                db.query("INSERT INTO telemetryData (componentTelemetryID, collectionDateTime, telemetryValue) VALUES (?, ?, ?)", insertParams, function(error, results, fields) {
                     if (error) throw error;
                 })
             }
