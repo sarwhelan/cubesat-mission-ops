8bfc88198c048dee8a5384158f78424241902b34
dzagar <10978376+dzagar@users.noreply.github.com>
2019-03-13 23:52:28 -0400

    Added backend error handling for telecommand batch service
    - Three retries, server error toast

--- frontend/mission-ops/src/app/services/telecommandBatch/telecommand-batch.service.ts
+++ frontend/mission-ops/src/app/services/telecommandBatch/telecommand-batch.service.ts
@@ -1,40 +1,65 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, never } from 'rxjs';
 import { TelecommandBatch } from 'src/classes/telecommandBatch';
 import { HttpClient } from '@angular/common/http';
 import { environment as env } from 'src/environments/environment';
-
+import { ToastrService } from 'ngx-toastr';
+import { retry, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TelecommandBatchService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private toastr: ToastrService) { }
 
   private telecommandBatchesUrl = `${env.apiRouteBase}/telecommand-batches`
 
   getTelecommandBatches() : Observable<TelecommandBatch[]>
   {
-    return this.http.get<TelecommandBatch[]>(this.telecommandBatchesUrl);
-  }
-
-  updateTelecommandBatch(batch: TelecommandBatch) : Observable<any>
-  {
-    var updateURL = this.telecommandBatchesUrl + "/" + batch.batchID;
-
-    return this.http.put(updateURL, batch);
-  }
-
-  deleteTelecommandBatch(batchID: number) : Observable<any>
-  {
-    var deleteURL = this.telecommandBatchesUrl + "/" + batchID;
-
-    return this.http.delete(deleteURL);
+    return this.http.get<TelecommandBatch[]>(this.telecommandBatchesUrl)
+            .pipe(
+              retry(3),
+              catchError(val => {
+                this.handleRequestError(val, "retrieving");
+                return never();
+              }));
   }
 
   createNewTelecommandBatch(newBatch: TelecommandBatch): Observable<any>
   {
-    return this.http.post(this.telecommandBatchesUrl, newBatch);
+    return this.http.post(this.telecommandBatchesUrl, newBatch)
+            .pipe(
+              retry(3),
+              catchError(val => {
+                this.handleRequestError(val, "creating");
+                return never();
+              }));
+  }
+
+  updateTelecommandBatch(batch: TelecommandBatch) : Observable<any>
+  {
+    return this.http.put(`${this.telecommandBatchesUrl}/${batch.batchID}`, batch)
+            .pipe(
+              retry(3),
+              catchError(val => {
+                this.handleRequestError(val, "updating");
+                return never();
+              }));
+  }
+
+  deleteTelecommandBatch(batchID: number) : Observable<any>
+  {
+    return this.http.delete(`${this.telecommandBatchesUrl}/${batchID}`)
+            .pipe(
+              retry(3),
+              catchError(val => {
+                this.handleRequestError(val, "deleting");
+                return never();
+              }));
+  }
+
+  private handleRequestError(error, eventType: string){
+    this.toastr.error(`Server error on ${eventType} Telecommand Batch: ${error.statusText} (Status ${error.status})`);
   }
 }
