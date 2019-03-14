f14e5bb2f5ca9411b1ae70cd2995bce935292ea8
dzagar <10978376+dzagar@users.noreply.github.com>
2019-03-14 00:32:59 -0400

    Added backend error handling for telemetry data service

--- frontend/mission-ops/src/app/services/component-telemetry-data/component-telemetry-data.service.ts
+++ frontend/mission-ops/src/app/services/component-telemetry-data/component-telemetry-data.service.ts
@@ -1,21 +1,29 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, never } from 'rxjs';
 import { TelemetryData } from 'src/classes/telemetry-data';
 import { HttpClient } from '@angular/common/http';
 import { environment as env } from 'src/environments/environment';
+import { ToastrService } from 'ngx-toastr';
+import { retry, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ComponentTelemetryDataService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private toastr: ToastrService) { }
 
   private telemetryDataUrl = `${env.apiRouteBase}/telemetry-data`;
 
   getTelemetryData(componentTelemetryID: number): Observable<TelemetryData[]> {
-    var getTelemetryDataUrl = this.telemetryDataUrl + "/" + componentTelemetryID;
-    return this.http.get<TelemetryData[]>(getTelemetryDataUrl);
+    return this.http.get<TelemetryData[]>(`${this.telemetryDataUrl}/${componentTelemetryID}`)
+      .pipe(
+        retry(3),
+        catchError(val => {
+          this.handleRequestError(val, "retrieving");
+          return never();
+        })
+      );
   }
 
   getTelemetryDataBetween(componentTelemetryID: number, startDate: Date, endDate: Date) : Observable<TelemetryData[]>
@@ -24,8 +32,18 @@ export class ComponentTelemetryDataService {
       'startDate': startDate.getTime().toString(),
       'endDate': endDate.getTime().toString(),
     }
-    console.log(params, `${this.telemetryDataUrl}/${componentTelemetryID}`);
 
-    return this.http.get<TelemetryData[]>(`${this.telemetryDataUrl}/${componentTelemetryID}`, {params: params});
+    return this.http.get<TelemetryData[]>(`${this.telemetryDataUrl}/${componentTelemetryID}`, {params: params})
+            .pipe(
+              retry(3),
+              catchError(val => {
+                this.handleRequestError(val, "retrieving");
+                return never();
+              })
+            );
+  }
+
+  private handleRequestError(error, eventType: string){
+    this.toastr.error(`Server error on ${eventType} Telemetry Data: ${error.statusText} (Status ${error.status})`);
   }
 }
