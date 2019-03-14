18ad0b988d1b2bf230557f915e561030605f9a2e
dzagar <10978376+dzagar@users.noreply.github.com>
2019-03-13 23:55:24 -0400

    Added backend error handling for telecommand service
    - Three retries, server error toast

--- frontend/mission-ops/src/app/services/telecommand/telecommand.service.ts
+++ frontend/mission-ops/src/app/services/telecommand/telecommand.service.ts
@@ -1,30 +1,55 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, never } from 'rxjs';
 import { Telecommand } from 'src/classes/telecommand';
 import { HttpClient } from '@angular/common/http';
 import { environment as env } from 'src/environments/environment';
+import { ToastrService } from 'ngx-toastr';
+import { retry, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TelecommandService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private toastr: ToastrService) { }
 
   private telecommandsUrl = `${env.apiRouteBase}/telecommands`
 
   getTelecommands() : Observable<Telecommand[]>
   {
-    return this.http.get<Telecommand[]>(this.telecommandsUrl);
-  }
-
-  deleteTelecommand(telecommandID : number) : Observable<any>{
-    var deleteUrl = this.telecommandsUrl + "/" + telecommandID;
-
-    return this.http.delete(deleteUrl);
+    return this.http.get<Telecommand[]>(this.telecommandsUrl)
+            .pipe(
+              retry(3),
+              catchError(val => {
+                this.handleRequestError(val, "retrieving");
+                return never();
+              })
+            );
   }
 
   createTelecommand(telecommand: Telecommand) : Observable<any>{
-    return this.http.post(this.telecommandsUrl, telecommand);
+    return this.http.post(this.telecommandsUrl, telecommand)
+            .pipe(
+              retry(3),
+              catchError(val => {
+                this.handleRequestError(val, "creating");
+                return never();
+              })
+            );
+  }
+
+  deleteTelecommand(telecommandID : number) : Observable<any>{
+    return this.http.delete(`${this.telecommandsUrl}/${telecommandID}`)
+            .pipe(
+              retry(3),
+              catchError(val => {
+                this.handleRequestError(val, "deleting");
+                return never();
+              })
+            );
+  }
+
+  private handleRequestError(error, eventType: string){
+    this.toastr.error(`Server error on ${eventType} Telecommand: ${error.statusText} (Status ${error.status})`);
   }
 }
