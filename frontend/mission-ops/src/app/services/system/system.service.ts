3206f07a636147abf76c1ff40672b91c57c76732
dzagar <10978376+dzagar@users.noreply.github.com>
2019-03-13 23:59:24 -0400

    Added backend error handling for system service
    - Three retries, server error toast
    - Cleaned up console logs

--- frontend/mission-ops/src/app/services/system/system.service.ts
+++ frontend/mission-ops/src/app/services/system/system.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, never } from 'rxjs';
 import { System } from 'src/classes/system';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment as env } from 'src/environments/environment';
+import { retry, catchError } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
@@ -15,30 +17,59 @@ export class SystemService {
     })
   };
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private toastr: ToastrService) { }
 
   private systemUrl = `${env.apiRouteBase}/systems`;
 
   getSystems(): Observable<System[]>
   {
-    console.log("getting systems in system service...");
-    var systems = this.http.get<System[]>(this.systemUrl);
-    console.log(systems);
-    return systems;
+    return this.http.get<System[]>(this.systemUrl)
+            .pipe(
+              retry(3),
+              catchError(val => {
+                this.handleRequestError(val, "retrieving");
+                return never();
+              })
+            );
   }
 
   createSystem(system: System): Observable<Number> 
   {
-    return this.http.post<Number>(this.systemUrl, JSON.stringify(system), this.httpOptions);
+    return this.http.post<Number>(this.systemUrl, JSON.stringify(system), this.httpOptions)
+            .pipe(
+              retry(3),
+              catchError(val => {
+                this.handleRequestError(val, "creating");
+                return never();
+              })
+            );
   }
 
   updateSystem(system: System): Observable<Number>
   {
-    return this.http.put<Number>(`${this.systemUrl}/${system.systemID}`, JSON.stringify(system), this.httpOptions);
+    return this.http.put<Number>(`${this.systemUrl}/${system.systemID}`, JSON.stringify(system), this.httpOptions)
+            .pipe(
+              retry(3),
+              catchError(val => {
+                this.handleRequestError(val, "updating");
+                return never();
+              })
+            );
   }
 
   removeSystem(system: System): Observable<System> 
   {
-    return this.http.delete<System>(`${this.systemUrl}/${system.systemID}`);
+    return this.http.delete<System>(`${this.systemUrl}/${system.systemID}`)
+            .pipe(
+              retry(3),
+              catchError(val => {
+                this.handleRequestError(val, "deleting");
+                return never();
+              })
+            );
+  }
+
+  private handleRequestError(error, eventType: string){
+    this.toastr.error(`Server error on ${eventType} System: ${error.statusText} (Status ${error.status})`);
   }
 }
