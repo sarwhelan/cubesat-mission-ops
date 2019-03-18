519ffe584c9db47b3faa4baa3f90e29df349d19d
Dana Zagar <10978376+dzagar@users.noreply.github.com>
2019-03-18 14:22:11 -0400

    Updated logger to log in all environments

--- ground_station/logger.js
+++ ground_station/logger.js
@@ -23,13 +23,11 @@ const logger = createLogger({
 	]
 });
 
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new transports.Console({
+logger.add(new transports.Console({
     format: format.combine(
       format.colorize(),
       format.simple()
     )
   }));
-}
 
 module.exports = logger;
