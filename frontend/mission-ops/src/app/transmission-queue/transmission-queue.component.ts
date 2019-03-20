a1eb4ec45b7977f590fbf50d6ba1dff6227465ff
dzagar <10978376+dzagar@users.noreply.github.com>
2019-03-20 15:19:38 -0400

    Docs for transmission queue

--- frontend/mission-ops/src/app/transmission-queue/transmission-queue.component.ts
+++ frontend/mission-ops/src/app/transmission-queue/transmission-queue.component.ts
@@ -1,36 +1,72 @@
 import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
-import { Pass } from 'src/classes/pass';
-import { PassService } from 'src/app/services/pass/pass.service';
-import { QueuedTelecommandService } from 'src/app/services/queuedTelecommand/queued-telecommand.service';
-import { QueuedTelecommand } from 'src/classes/queuedTelecommand';
-import { Observable, of, empty } from 'rxjs';
-import { Telecommand } from 'src/classes/telecommand';
-import { User } from 'src/classes/user';
-import { UsersService } from 'src/app/services/users/users.service';
+import { empty } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { environment as env } from 'src/environments/environment';
 import { ToastrService } from 'ngx-toastr';
 import { retry, catchError } from 'rxjs/operators';
 
+import { environment as env } from 'src/environments/environment';
+import { Pass } from 'src/classes/pass';
+import { QueuedTelecommandService } from 'src/app/services/queuedTelecommand/queued-telecommand.service';
+import { QueuedTelecommand } from 'src/classes/queuedTelecommand';
+import { Telecommand } from 'src/classes/telecommand';
+import { User } from 'src/classes/user';
+import { UsersService } from 'src/app/services/users/users.service';
+
+/**
+ * The component housing the transmission queue.
+ * 
+ * @class TransmissionQueueComponent
+ */
 @Component({
   selector: 'app-transmission-queue',
   templateUrl: './transmission-queue.component.html',
   styleUrls: ['./transmission-queue.component.scss']
 })
+
 export class TransmissionQueueComponent implements OnInit {
 
+  /**
+   * The selected pass for which to display the queue.
+   * 
+   * @memberof TransmissionQueueComponent
+   */
   @Input() selectedPass: Pass;
+
+  /**
+   * The telecommands to pass through to the queued telecommand component.
+   * 
+   * @memberof TransmissionQueueComponent
+   */
   @Input() telecommands: Telecommand[];
   
+  /**
+   * The collection of queued telecommands for the specified pass.
+   * 
+   * @memberof TransmissionQueueComponent
+   */
   private passQueuedTelecommands: QueuedTelecommand[];
+
+  /**
+   * The collection of users.
+   * 
+   * @memberof TransmissionQueueComponent
+   */
   users: User[]
 
+  /**
+   * Constructs a new instance of the TransmissionQueueComponent component.
+   * @param userService 
+   * @param queuedTelecommandService 
+   * @param http 
+   * @param toastr 
+   */
   constructor(private userService: UsersService, 
     private queuedTelecommandService: QueuedTelecommandService,
     private http: HttpClient,
     private toastr: ToastrService) { }
 
   ngOnInit() {
+    // Retrieves all users.
     this.userService.getUsers(Number.MAX_SAFE_INTEGER - 1)
       .subscribe(users => {
         this.users = users.items
@@ -40,17 +76,30 @@ export class TransmissionQueueComponent implements OnInit {
 
   ngOnChanges(changes: SimpleChanges) : void
   {
+    // If the pass selection changes after the initial load, reload
+    // queued telecommands.
     if (changes.selectedPass && !changes.selectedPass.firstChange)
     {
       this.reloadQueuedTelecommands();
     }
   }
 
+  /**
+   * Reloads queued telecommands based on the transmission pass ID given.
+   * 
+   * @memberof TransmissionQueueComponent
+   */
   reloadQueuedTelecommands(){
     this.queuedTelecommandService.getQueuedTelecommandsTransmission(this.selectedPass)
       .subscribe(qtc => this.passQueuedTelecommands = qtc);
   }
 
+  /**
+   * Submits the current collection of queued telecommands to the ground station
+   * server via POST.
+   * 
+   * @memberof TransmissionQueueComponent
+   */
   submitTransmissionQueueToGroundStation(){
       this.http.post(`${env.groundStationIP}/queue/${this.selectedPass.passID}`, { queue: this.passQueuedTelecommands})
       .pipe(
@@ -64,6 +113,10 @@ export class TransmissionQueueComponent implements OnInit {
       });
   }
 
+  /**
+   * The error handling method called when there was an error during a service request.
+   * @param error The error object associated with this mighty failure.
+   */
   handleSubmitError(error){
     this.toastr.error(`Error transmitting to the ground station: ${error.statusText} (Status Code ${error.status})`, "Ground station server error!");
   }
