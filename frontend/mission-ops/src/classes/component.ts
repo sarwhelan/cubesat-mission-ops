d76175f32e058b15b65e17685c9d98285da9b275
dzagar <10978376+dzagar@users.noreply.github.com>
2019-03-21 12:48:32 -0400

    Documenting pass sum and component classes

--- frontend/mission-ops/src/classes/component.ts
+++ frontend/mission-ops/src/classes/component.ts
@@ -1,8 +1,33 @@
 export class Component {
+    /**
+     * The unique ID.
+     * 
+     * @type {number}
+     * @memberof Component
+     */
     componentID: number;
+
+    /**
+     * The ID of the @alias System associated.
+     * 
+     * @type {number}
+     * @memberof Component
+     */
     systemID: number;
+
+    /**
+     * The name.
+     * 
+     * @type {string}
+     * @memberof Component
+     */
     name: string;
 
+    /**
+     * Constructs a new instance of a @class Component.
+     * @param systemID The associated System.
+     * @param name The name.
+     */
     constructor(systemID: number, name: string) {
         this.systemID = systemID;
         this.name = name;
--- frontend/mission-ops/src/classes/pass-sum.ts
+++ frontend/mission-ops/src/classes/pass-sum.ts
@@ -1,8 +1,36 @@
 export class PassSum {
+    /**
+     * The ID of the Pass being
+     * summed over.
+     * 
+     * @type {number}
+     * @memberof PassSum
+     */
     passID: number;
+
+    /**
+     * The sum of the bandwidth usage
+     * contained in the Pass.
+     * 
+     * @type {number}
+     * @memberof PassSum
+     */
     sumBandwidth: number;
+
+    /**
+     * The sum of the pwer consumption
+     * contained in the Pass.
+     * 
+     * @type {number}
+     * @memberof PassSum
+     */
     sumPower: number;
 
+    /**
+     * Constructs a new instance of a @class PassSum.
+     * @param sumBandwidth The sum of the bandwidth in the pass.
+     * @param sumPower The sum of the power in the pass.
+     */
     constructor(sumBandwidth: number, sumPower: number)
     {
         this.sumBandwidth = sumBandwidth;
